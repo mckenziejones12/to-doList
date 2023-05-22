dd07fee90749b4da308bc5ee83e3cf68098c2b50
McKenzie Jones <mckenzbeck12@gmail.com>
2023-05-22 14:57:09 -0600

    Create nav bar

--- src/index.js
+++ src/index.js
@@ -3,5 +3,36 @@ console.log("hello world");
 
 // Create project tabs in the nav bar that show extended
 // to -do list in content section when clicked
+function navBarLoad() {
+  const navBar = document.getElementById("navBar");
+  const content = document.getElementById("content");
 
-//
+  const navBarContainer = document.createElement("div");
+  navBarContainer.setAttribute("id", "navBarContainer");
+  const navBarHeader = document.createElement("div");
+  navBarHeader.setAttribute("id", "navBarHeader");
+  navBarHeader.textContent = "Projects";
+  const navBarTabs = document.createElement("div");
+  navBarTabs.setAttribute("id", "navBarTabs");
+  const weddingTab = document.createElement("button");
+  weddingTab.setAttribute("class", "navBarTab");
+  weddingTab.textContent = "Wedding";
+  const webDevTab = document.createElement("button");
+  webDevTab.setAttribute("class", "navBarTab");
+  webDevTab.textContent = "Web Dev";
+  const addNewProjectTab = document.createElement("button");
+  addNewProjectTab.setAttribute("class", "navBarTab");
+  addNewProjectTab.setAttribute("id", "addNewProjectTab");
+  addNewProjectTab.textContent = " + New Project";
+  navBarTabs.appendChild(weddingTab);
+  navBarTabs.appendChild(webDevTab);
+  navBarTabs.appendChild(addNewProjectTab);
+  navBarContainer.appendChild(navBarHeader);
+  navBarContainer.appendChild(navBarTabs);
+
+  navBar.appendChild(navBarContainer);
+}
+
+navBarLoad();
+
+//show a default to-do list
